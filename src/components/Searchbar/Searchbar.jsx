5a8841059532e5456b2ecd2c38c9ff8bd71333cd
Łukasz <lukaszkogut1996@gmail.com>
2023-06-06 18:18:03 +0200

    add SearchBar + propTypes

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,9 +1,41 @@
-import { Component } from "react";
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 
-export class SearchBar extends Component {
-  state = {
-    query: '',
+const Searchbar = ({ onSubmit }) => {
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSubmit(searchValue);
   };
 
-  
-}
+  const handleChange = e => {
+    setSearchValue(e.target.value);
+  };
+
+  return (
+    <header className="searchbar">
+      <form className="form" onSubmit={handleSubmit}>
+        <button type="submit" className="button">
+          <span className="button-label">Search</span>
+        </button>
+
+        <input
+          className="input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={searchValue}
+          onChange={handleChange}
+        />
+      </form>
+    </header>
+  );
+};
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default Searchbar;
