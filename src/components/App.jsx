680b0d577646cfa28e22df012a8ef596a392302b
Łukasz <lukaszkogut1996@gmail.com>
2023-06-06 21:13:56 +0200

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
-export const App = () => {
+import React, { useState, useEffect, useCallback } from 'react';
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Button from './Button/Button';
+import Loader from './Loader/Loader';
+import Modal from './Modal/Modal';
+
+const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const fetchImages = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=29601510-cb62b0e552e92b2824471424b&image_type=photo&orientation=horizontal&per_page=12`
+      );
+      const data = await response.json();
+      setImages(prevImages =>
+        page === 1 ? data.hits : [...prevImages, ...data.hits]
+      );
+    } catch (error) {
+      console.log('Error fetching images:', error);
+    }
+    setLoading(false);
+  }, [searchQuery, page]);
+
+  useEffect(() => {
+    fetchImages();
+  }, [fetchImages]);
+
+  const handleSearchSubmit = value => {
+    setSearchQuery(value);
+    setPage(1);
+  };
+
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+
+  const handleImageClick = image => {
+    setSelectedImage(image);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="app">
+      <Searchbar onSubmit={handleSearchSubmit} />
+      <ImageGallery images={images} onItemClick={handleImageClick} />
+      {loading && <Loader />}
+      {images.length > 0 && <Button onClick={handleLoadMore} />}
+      {selectedImage && (
+        <Modal image={selectedImage} onClose={handleCloseModal} />
+      )}
     </div>
   );
 };
+
+export default App;
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,22 +1,27 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
+import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
 
-const ImageGallery = ({ images, onItemClick }) => {
-  return (
-    <ul className="gallery">
-      {images.map(image => (
-        <ImageGalleryItem
-          key={image.id}
-          image={image}
-          onItemClick={onItemClick}
-        />
-      ))}
-    </ul>
-  );
-};
+const ImageGallery = ({ images, onItemClick }) => (
+  <ul className="gallery">
+    {images.map((image) => (
+      <ImageGalleryItem
+        key={image.id}
+        image={image}
+        onItemClick={onItemClick}
+      />
+    ))}
+  </ul>
+);
 
 ImageGallery.propTypes = {
-  images: PropTypes.array.isRequired,
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      webformatURL: PropTypes.string.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
+    })
+  ).isRequired,
   onItemClick: PropTypes.func.isRequired,
 };
 
